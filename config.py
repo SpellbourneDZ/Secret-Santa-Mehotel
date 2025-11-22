4046f871a7e7d48e69d2160d2b91c85fb3e326fe
SpellbourneDZ <125334786+SpellbourneDZ@users.noreply.github.com>
2025-11-22 09:53:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
-# config.py
+import os
 
-BOT_TOKEN = "8559551039:AAHIXaeGLeVMRCZBg0n8wDIqyNsgTCpQyFM"  # <-- сюда вставь токен бота от @BotFather
-
-# ID админов (как минимум твой own user_id)
-ADMINS = [
-    409873627,  # заменишь на свой Telegram ID
-]
-
-# Путь к SQLite-базе
+BOT_TOKEN = os.getenv("8559551039:AAHIXaeGLeVMRCZBg0n8wDIqyNsgTCpQyFM")
+ADMINS = [int(x) for x in os.getenv("ADMINS", "409873627").split(",") if x]
 DB_PATH = "secret_santa.db"
-
-# Дата раскрытия в текстах
 REVEAL_DATE = "19 декабря"
