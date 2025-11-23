bdce3b88e4696c7f970f7e1631ad9418481669ac
SpellbourneDZ <125334786+SpellbourneDZ@users.noreply.github.com>
2025-11-23 16:31:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMINS = [int(x) for x in os.getenv("ADMINS", "").split(",") if x]
 DB_PATH = "secret_santa.db"
-REVEAL_DATE = "19 декабря"
+REVEAL_DATE = "1 декабря"
+
